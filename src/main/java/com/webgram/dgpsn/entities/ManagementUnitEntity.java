bacbda01ad2612fc297f2a6dc89338ef1968e04e
Laty <latysamba@gmail.com>
2025-10-28 13:02:43 +0000

    objectifs->actions->activtes->indicateurs

--- src/main/java/com/webgram/dgpsn/entities/ManagementUnitEntity.java
+++ src/main/java/com/webgram/dgpsn/entities/ManagementUnitEntity.java
@@ -1,5 +1,6 @@
 package com.webgram.dgpsn.entities;
 
+import com.webgram.dgpsn.models.StructureDTO;
 import jakarta.persistence.*;
 import lombok.*;
 import com.webgram.dgpsn.entities.audits.Auditable;
@@ -118,10 +119,22 @@ public class ManagementUnitEntity extends Auditable<Long> implements Serializabl
     private List<LabelEntity> executionZones;
 
     @ManyToMany
+    @JoinTable(name = "management_unit_verificationSources",
+            joinColumns = {@JoinColumn(name = "management_unit_id")},
+            inverseJoinColumns = {@JoinColumn(name = "verificationSources_id")})
+    private List<LabelEntity> verificationSources;
+
+    @ManyToMany
     @JoinTable(name = "management_unit_beneficiary",
             joinColumns = {@JoinColumn(name = "management_unit_id")},
             inverseJoinColumns = {@JoinColumn(name = "beneficiary_id")})
     private List<LabelEntity> beneficiaries;
+
+    @ManyToMany
+    @JoinTable(name = "management_unit_actorsInvolved",
+            joinColumns = {@JoinColumn(name = "management_unit_id")},
+            inverseJoinColumns = {@JoinColumn(name = "actorsInvolved_id")})
+    private List<StructureEntity> actorsInvolved;
 
     @ManyToOne
     private StructureEntity structure;
