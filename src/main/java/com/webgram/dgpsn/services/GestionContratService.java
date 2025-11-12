e66dd903d21273fcd9c8f7316c20f8105cc39711
thatsbass <thatsbass.dev@outlook.com>
2025-11-12 17:55:15 +0000

    Add gestion contrat

---
+++ src/main/java/com/webgram/dgpsn/services/GestionContratService.java
@@ -0,0 +1,18 @@
+package com.webgram.dgpsn.services;
+
+import com.webgram.dgpsn.models.GestionContratDTO;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface GestionContratService {
+    GestionContratDTO create(GestionContratDTO dto);
+    @Transactional
+    GestionContratDTO updateStatusContrat(Long contratId, boolean isActive);
+    @Transactional(readOnly = true)
+    List<GestionContratDTO> findByAgentId(Long agentId);
+    GestionContratDTO update(Long contratId, GestionContratDTO dto);
+    void delete(Long contratId);
+    List<GestionContratDTO> searchByAgentId(Long agentId, String typeContratId, LocalDate dateDebut, LocalDate dateFin);
+}
